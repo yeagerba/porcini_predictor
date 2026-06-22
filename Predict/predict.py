e7646c717e6716a46ec3607e8200cdc0baeb1db4
Ben Yeager <ben@Bens-MacBook-Air.local>
2026-06-22 16:40:44 -0700

    Added original values of perfect day features back to the prediction dataframe to help with display of forecast weather alongside porcini forecast

--- Predict/predict.py
+++ Predict/predict.py
@@ -102,6 +102,10 @@ def make_predictions():
 
     prediction_df['predicted_count'] = predictions
 
+    # Bring back true values of perfect day features (useful for displaying, e.g. rain along with porcini forecast)
+    for feature in perfect_day_features.keys():
+        prediction_df[feature + '_true'] = weather_df[feature]
+
     return prediction_df
 
 
