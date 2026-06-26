c4fc04c5a2d03bb5e1bd29d7e607091059adebb2
Ben Yeager <ben@Bens-MacBook-Air.local>
2026-06-26 15:45:23 -0700

    Cosmetic improvements, added photos

--- webapp_streamlit/app.py
+++ webapp_streamlit/app.py
@@ -24,20 +24,11 @@ st.title("Point Reyes Porcini Forecast")
 
 # --- TABS ---
 # tab1, tab2, tab3 = st.tabs(["Forecast", "Where to Look", "Methodology (Data Science)"])
-tab1, tab2 = st.tabs(["Forecast", "Where to Look"])
+tab1, tab2 = st.tabs(["Forecast", "About Porcini & Where to Find Them"])
 
 # --- TAB 1: FORECAST ---
 with tab1:
-    st.header("OVERVIEW")
-    # st.info("This section will host the Temporal Model.")
-    
-    # st.markdown("""
-    # **Upcoming Logic:**
-    # 1. **Fetch Live Weather:** Pulling daily rain/temp from NOAA for Point Reyes Station.
-    # 2. **Calculate Features:** * *30-Day Cumulative Rain*
-    #     * *Soil Temp Shock (7-day delta)*
-    # 3. **Logistic Regression:** Probability Score (0-100%).
-    # """)
+    # st.header("OVERVIEW")
 
     # Load the predictions
     predictions_df = pd.read_csv('predictions.csv')
@@ -55,220 +46,228 @@ with tab1:
         70: 'HIGH',
         100: 'VERY HIGH'
     }
-    infotext = "Likelihood of porcini fruiting is currently "
+    likelihood_text = "Likelihood of porcini fruiting is currently "
     for key, value in likelihood_dict.items():
         if predictions_df['porcini_index'].iloc[0] <= key:
-            infotext += value
+            likelihood_text += value
             current_likelihood = value
             break
-    infotext += ".\n\n"
-    infotext += "Maximum likelihood of porcini fruiting within the next 14 days is "
+    likelihood_text += ".\n\n"
+    likelihood_text += "Likelihood of porcini fruiting within the next two weeks is "
     for key, value in likelihood_dict.items():
         if predictions_df['porcini_index'].max() <= key:
-            infotext += value
+            likelihood_text += value
             forecast_likelihood = value
             break
-    infotext += ".\n\n"
+    likelihood_text += ".\n\n"
 
     if current_likelihood == 'LOW' and forecast_likelihood == 'LOW' and predictions_df['date'].iloc[0].month in [4, 5, 6, 7, 8, 9]:
-        infotext = "**Porcini season in Point Reyes is typically from October to February. Check back later!**\n\n" + infotext
+        infotext = "**No porcini expected in forecast period. Porcini season in Point Reyes runs from Oct-Feb. Check back later!**"
    
     
-    st.info(infotext)
+    st.info(infotext, icon="😴")
+    st.text(likelihood_text)
 
 
-    st.header("FORECAST")
-
-    # # Load the forecast chart from the JSON file
-    # with open('visualizations/forecast_chart.json', 'r') as f:
-    #     chart_dict = json.load(f)
-    
-    # # Remove any hardcoded top-level width
-    # chart_dict.pop('width', None)
-    
-    # # Remove hidden config-level widths that override container settings
-    # if 'config' in chart_dict and 'view' in chart_dict['config']:
-    #     chart_dict['config']['view'].pop('continuousWidth', None)
-    #     chart_dict['config']['view'].pop('width', None)
-        
-    # # Prevent padding from pushing the container width past 100%
-    # chart_dict['padding'] = {
-    #     "left": 10,
-    #     "right": 1000, # Increase this if the right side is still spilling over
-    #     "top": 10,
-    #     "bottom": 10
-    # }
-    # chart_dict['autosize'] = {"type": "fit", "contains": "padding"}
-    
-    # # Apply responsive width to the individual sub-charts
-    # if 'vconcat' in chart_dict:
-    #     for subchart in chart_dict['vconcat']:
-    #         subchart['width'] = 'container'
-
-    # st.vega_lite_chart(chart_dict, width='stretch') #, theme='streamlit')
+    # st.header("FORECAST")
 
 
     # ================================================
     # FORECAST CHART
     # ================================================
+
     predictions_df = pd.read_csv('predictions.csv')
-    # Create a base layer with the shared X-axis (formatted as Day-Date, no vertical grid)
-    base = alt.Chart(predictions_df).encode(
-        x=alt.X('date:T', axis=alt.Axis(format='%a %d', title=None, grid=False, labelAngle=-45, tickCount=14))
-    )
 
-    # Define bands for background annotation (for colored zones only, no text labels on bands)
-    bands = [
-        {"y0": 0, "y1": 10, "color": "#CFD6EA"},
-        {"y0": 10, "y1": 40, "color": "#EDF7ED"},
-        {"y0": 40, "y1": 70, "color": "#D3EBCD"},
-        {"y0": 70, "y1": 100, "color": "#AED5A2"},
-    ]
-    band_chart = alt.Chart(pd.DataFrame(bands)).mark_rect().encode(
-        y='y0:Q',
-        y2='y1:Q',
-        color=alt.Color('color:N', scale=None, legend=None),
-    ).properties(
-        # width=alt.Step(30),
-        height=150
-    )
+    # Create two columns to prevent the chart from overflowing the screen
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        # To label dates only on the top (porcini) and bottom (rain) charts,
+        # we set the x-axis for those charts, and omit from temp.
 
-    # Add thin dark gray horizontal lines at each y tick
-    yticks = [0, 10, 40, 70, 100]
-    ytick_labels = {
-        0: "LOW",
-        10: "ELEVATED",
-        40: "HIGH",
-        70: "VERY HIGH",
-        100: ""
-    }
-    lines_df = pd.DataFrame({'y': yticks})
-    lines_chart = alt.Chart(lines_df).mark_rule(strokeWidth=1, color='#444', opacity=0.65).encode(
-        y='y:Q'
-    ).properties(
-        # width=alt.Step(30),
-        height=150
-    )
-
-    porcini_line = base.mark_line(
-        color='#5C3A21', strokeWidth=3, interpolate='monotone'
-    ).encode(
-        y=alt.Y(
-            'porcini_index:Q',
-            title='Fruiting Likelihood',
-            scale=alt.Scale(domain=[0, 100]),
-            axis=alt.Axis(
-                grid=False,
-                values=yticks,
-                tickCount=len(yticks),
-                ticks=True,
-                labels=True,
-                labelExpr=f"""
-                    {{
-                        0: '{ytick_labels[0]}',
-                        10: '{ytick_labels[10]}',
-                        40: '{ytick_labels[40]}',
-                        70: '{ytick_labels[70]}',
-                        100: '{ytick_labels[100]}'
-                    }}[datum.value] || ''
-                """,
-                labelBaseline="bottom",   # Move the labels up above the ticks
-                labelPadding=0            # Reduce padding (default is 4), move closer to axis (and up)
-            )
+        # Shared X spec for charts with labels at top
+        date_x_axis_top = alt.X(
+            'date:T',
+            axis=alt.Axis(format='%a %d', title=None, grid=False, labelAngle=-45, tickCount=14, orient='top')
         )
-    ).properties(height=150)
-
-    porcini_points = base.mark_point(
-        color='#5C3A21', filled=True, size=60
-    ).encode(
-        y=alt.Y('porcini_index:Q')
-    ).properties(height=150)
-
-    porcini = band_chart + lines_chart + porcini_line + porcini_points
-
-    # Create a DataFrame for grid lines at 0, 5, 10, 15, 20, 25
-    temp_grid_y = [0, 5, 10, 15, 20, 25]
-    temp_grid_df = pd.DataFrame({'y': temp_grid_y})
-    temp_grid_chart = alt.Chart(temp_grid_df).mark_rule(
-        strokeWidth=1,
-        color='#333',
-        opacity=0.45
-    ).encode(
-        y='y:Q'
-    ).properties(
-        height=100
-    )
-
-    temp = (
-        temp_grid_chart
-        + base.mark_line(
-            color='#FF4B4B', strokeWidth=1.5, interpolate='monotone'
-        ).encode(
-            y=alt.Y(
-                'tmax_c_true:Q',
-                title='Temp (°C)',
-                scale=alt.Scale(domain=[0, 25]),
-                axis=alt.Axis(grid=False)
-            )
-        ).properties(height=100)
-        + base.mark_point(
-            color='#FF4B4B', filled=True, size=30
-        ).encode(
-            y=alt.Y('tmax_c_true:Q', scale=alt.Scale(domain=[0, 25]))
-        ).properties(height=100)
-        + base.mark_line(
-            color='#0077B6', strokeWidth=1.5, interpolate='monotone'
-        ).encode(
-            y=alt.Y(
-                'tmin_c_true:Q',
-                title='Temp (°C)',
-                scale=alt.Scale(domain=[0, 25]),
-                axis=alt.Axis(grid=False)
-            )
-        ).properties(height=100)
-        + base.mark_point(
-            color='#0077B6', filled=True, size=30
-        ).encode(
-            y=alt.Y('tmin_c_true:Q', scale=alt.Scale(domain=[0, 25]))
-        ).properties(height=100)
-    )
-
-    # Set rain y-axis domain: [0, max(10, prcp_mm_true.max())]
-    rain_y_max = max(10, predictions_df['prcp_mm_true'].max())
-    rain = base.mark_bar(
-        color='#3A86FF', opacity=0.8
-    ).encode(
-        y=alt.Y(
-            'prcp_mm_true:Q',
-            title='Rain (mm)',
-            scale=alt.Scale(domain=[0, rain_y_max]),
-            axis=alt.Axis(grid=False)
+        # Shared X spec for charts with labels at bottom
+        date_x_axis_bottom = alt.X(
+            'date:T',
+            axis=alt.Axis(format='%a %d', title=None, grid=False, labelAngle=-45, tickCount=14, orient='bottom')
         )
-    ).properties(height=100)
+        # Shared X spec for charts without labels
+        date_x_no_labels = alt.X(
+            'date:T',
+            axis=alt.Axis(labels=False, ticks=False, title=None, grid=False)
+        )
 
-    # If no rain is forecasted at all, overlay gray text
-    if predictions_df['prcp_mm_true'].max() == 0:
-        no_rain_text = alt.Chart(pd.DataFrame({'x': [predictions_df['date'].iloc[len(predictions_df)//2]], 'y': [rain_y_max/2]})).mark_text(
-            text="No rain in forecast",
-            color='gray',
-            size=18,
-            fontWeight='bold'
-        ).encode(
-            x='x:T',
+        # Create a base layer for the porcini chart (with x labels at top)
+        base_top = alt.Chart(predictions_df).encode(
+            x=date_x_axis_top
+        )
+        # Create a base layer for the mid chart (no x labels)
+        base_mid = alt.Chart(predictions_df).encode(
+            x=date_x_no_labels
+        )
+        # Create a base layer for the bottom/rain chart (x labels at bottom)
+        base_bottom = alt.Chart(predictions_df).encode(
+            x=date_x_axis_bottom
+        )
+
+        # Define bands for background annotation (for colored zones only, no text labels on bands)
+        bands = [
+            {"y0": 0, "y1": 10, "color": "#CFD6EA"},
+            {"y0": 10, "y1": 40, "color": "#EDF7ED"},
+            {"y0": 40, "y1": 70, "color": "#D3EBCD"},
+            {"y0": 70, "y1": 100, "color": "#AED5A2"},
+        ]
+        band_chart = alt.Chart(pd.DataFrame(bands)).mark_rect().encode(
+            y='y0:Q',
+            y2='y1:Q',
+            color=alt.Color('color:N', scale=None, legend=None),
+        ).properties(
+            width='container',
+            height=150
+        )
+
+        # Add thin dark gray horizontal lines at each y tick
+        yticks = [0, 10, 40, 70, 100]
+        ytick_labels = {
+            0: "LOW",
+            10: "ELEVATED",
+            40: "HIGH",
+            70: "VERY HIGH",
+            100: ""
+        }
+        lines_df = pd.DataFrame({'y': yticks})
+        lines_chart = alt.Chart(lines_df).mark_rule(strokeWidth=1, color='#444', opacity=0.65).encode(
             y='y:Q'
-        ).properties(height=100)
-        rain = rain + no_rain_text
+        ).properties(
+            width='container',
+            height=150
+        )
 
-    # Stack them vertically, share the X-axis, and remove outer borders
-    forecast_chart = alt.vconcat(
-        porcini, temp, rain, spacing=10
-    ).resolve_scale(
-        x='shared' # This forces them to align perfectly
-    ).configure_view(
-        strokeOpacity=0 # Removes the box around the charts
-    )
+        porcini_line = base_top.mark_line(
+            color='#5C3A21', strokeWidth=3, interpolate='monotone'
+        ).encode(
+            y=alt.Y(
+                'porcini_index:Q',
+                title='Fruiting Likelihood',
+                scale=alt.Scale(domain=[0, 100]),
+                axis=alt.Axis(
+                    grid=False,
+                    values=yticks,
+                    tickCount=len(yticks),
+                    ticks=True,
+                    labels=True,
+                    labelExpr=f"""
+                        {{
+                            0: '{ytick_labels[0]}',
+                            10: '{ytick_labels[10]}',
+                            40: '{ytick_labels[40]}',
+                            70: '{ytick_labels[70]}',
+                            100: '{ytick_labels[100]}'
+                        }}[datum.value] || ''
+                    """,
+                    labelBaseline="bottom",   # Move the labels up above the ticks
+                    labelPadding=0            # Reduce padding (default is 4), move closer to axis (and up)
+                )
+            )
+        ).properties(height=150)
 
-    st.altair_chart(forecast_chart, width='stretch')
+        porcini_points = base_top.mark_point(
+            color='#5C3A21', filled=True, size=60
+        ).encode(
+            y=alt.Y('porcini_index:Q')
+        ).properties(height=150)
+
+        porcini = band_chart + lines_chart + porcini_line + porcini_points
+
+        # Create a DataFrame for grid lines at 0, 5, 10, 15, 20, 25
+        temp_grid_y = [0, 5, 10, 15, 20, 25]
+        temp_grid_df = pd.DataFrame({'y': temp_grid_y})
+        temp_grid_chart = alt.Chart(temp_grid_df).mark_rule(
+            strokeWidth=1,
+            color='#333',
+            opacity=0.45
+        ).encode(
+            y='y:Q'
+        ).properties(
+            height=150
+        )
+
+        temp = (
+            (temp_grid_chart
+            + base_mid.mark_line(
+                color='#FF4B4B', strokeWidth=1.5, interpolate='monotone'
+            ).encode(
+                y=alt.Y(
+                    'tmax_c_true:Q',
+                    title='Temp (°C)',
+                    scale=alt.Scale(domain=[0, 25]),
+                    axis=alt.Axis(grid=False)
+                )
+            ).properties(height=150)
+            + base_mid.mark_point(
+                color='#FF4B4B', filled=True, size=30
+            ).encode(
+                y=alt.Y('tmax_c_true:Q', scale=alt.Scale(domain=[0, 25]))
+            ).properties(height=150)
+            + base_mid.mark_line(
+                color='#0077B6', strokeWidth=1.5, interpolate='monotone'
+            ).encode(
+                y=alt.Y(
+                    'tmin_c_true:Q',
+                    title='Temp (°C)',
+                    scale=alt.Scale(domain=[0, 25]),
+                    axis=alt.Axis(grid=False)
+                )
+            ).properties(height=150)
+            + base_mid.mark_point(
+                color='#0077B6', filled=True, size=30
+            ).encode(
+                y=alt.Y('tmin_c_true:Q', scale=alt.Scale(domain=[0, 25]))
+            ).properties(height=150)
+            )
+        )
+
+        # Set rain y-axis domain: [0, max(10, prcp_mm_true.max())]
+        rain_y_max = max(10, predictions_df['prcp_mm_true'].max())
+        rain = base_bottom.mark_bar(
+            color='#3A86FF', opacity=0.8
+        ).encode(
+            y=alt.Y(
+                'prcp_mm_true:Q',
+                title='Rain (mm)',
+                scale=alt.Scale(domain=[0, rain_y_max]),
+                axis=alt.Axis(grid=False)
+            )
+        ).properties(height=150)
+
+        # If no rain is forecasted at all, overlay gray text
+        if predictions_df['prcp_mm_true'].max() == 0:
+            no_rain_text = alt.Chart(pd.DataFrame({'x': [predictions_df['date'].iloc[len(predictions_df)//2]], 'y': [rain_y_max/2]})).mark_text(
+                text="No rain in forecast",
+                color='gray',
+                size=18,
+                fontWeight='bold'
+            ).encode(
+                x='x:T',
+                y='y:Q'
+            ).properties(height=150)
+            rain = rain + no_rain_text
+
+        # Stack them vertically, share the X-axis, and remove outer borders.
+        # View config must be on the vconcat chart, not on subcharts.
+        forecast_chart = alt.vconcat(
+            porcini, temp, rain, spacing=10
+        ).resolve_scale(
+            x='shared' # This forces them to align perfectly
+        ).configure_view(
+            fill='white',
+            strokeOpacity=0 # Removes the box around the charts
+        )
+
+        st.altair_chart(forecast_chart, width='stretch')
     # =========================================================
     # END FORECAST CHART
     # =========================================================
@@ -276,26 +275,33 @@ with tab1:
 
     st.divider()
 
-# --- TAB 2: WHERE TO LOOK ---
 with tab2:
-    st.header("Where to find porcini")
+    st.header("About Porcini")
 
-    st.text("""
-    Porcini mushrooms are mycorrhizal, meaning they grow in relationship with trees, sharing nutrients and water. 
-    In Point Reyes, they are most commonly found in association with Bishop Pine *Pinus muricata*, though they are also often found with Douglas Fir *Pseudotsuga menziesii*.
+    col1, col2 = st.columns([2, 4])
 
-    The map below shows the locations of Bishop Pine (in green) and Douglas Fir (in blue) in Point Reyes. 
-    Dotted lines represent trails - find a trail that runs through Bishop Pine habitat for your best chance to find porcini!
+    with col1:
+        st.image("images/big_porcini.jpg", width=200)
+        st.image("images/underside.jpg", width=200)
+        st.image("images/in_ground.jpg", width=200)
+        st.image("images/baby.jpg", width=200)
+    with col2:
+        st.text("""
+        Porcini mushrooms are mycorrhizal, meaning they grow in relationship with trees, sharing nutrients and water. 
+        In Point Reyes, they are most commonly found in association with Bishop Pine *Pinus muricata*, though they are also often found with Douglas Fir *Pseudotsuga menziesii*.
 
-    Note also the red region denoting the area affected by the 2020 Woodward Fire. The forest in this area is still recovering and has large areas of thick undergrowth that can make it difficult to find porcini.
-    """)
+        The map below shows the locations of Bishop Pine (in green) and Douglas Fir (in blue) in Point Reyes. 
+        Dotted lines represent trails - find a trail that runs through Bishop Pine habitat for your best chance to find porcini!
 
-    # Load the map HTML file
-    with open("visualizations/vegetation_trail_map.html", "r", encoding="utf-8") as f:
-        map_html = f.read()
+        Note also the red region denoting the area affected by the 2020 Woodward Fire. The forest in this area is still recovering and has large areas of thick undergrowth that can make it difficult to find porcini.
+        """)
 
-    # Display the map using Streamlit's HTML component
-    html(map_html, height=600, width=1000)
+        # Load the map HTML file
+        with open("visualizations/vegetation_trail_map.html", "r", encoding="utf-8") as f:
+            map_html = f.read()
+
+        # Display the map using Streamlit's HTML component
+        html(map_html, height=600, width=1000)
 
 # --- TAB 3: METHODOLOGY ---
 # with tab3:
@@ -311,9 +317,9 @@ with tab2:
 #     *(To be populated after we build your Logistic Regression model)*
 #     """)
 
-# --- SIDEBAR ---
-with st.sidebar:
-    st.subheader("Project Info")
-    st.write("Created by **Ben Yeager**")
-    st.write("Tools: Python, GeoPandas, Folium, Streamlit")
-    st.button("Contact Me") # Link to LinkedIn
+# # --- SIDEBAR ---
+# with st.sidebar:
+#     st.subheader("Project Info")
+#     st.write("Created by **Ben Yeager**")
+#     st.write("Tools: Python, GeoPandas, Folium, Streamlit")
+#     st.button("Contact Me") # Link to LinkedIn
